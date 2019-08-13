638d44340990a888332b1c538eeeed41ab830768
suyog <suyogpradhan711@gmail.com>
2019-08-13 15:49:28 +0545

    added routes and queries for department and city

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var config = require('./config/config');
+var cityRoute = require('./components/city/city.routes');
+var deptRoute = require('./components/department/dept.routes');
 require('./config/db.config');
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var app = express();
 app.listen(config.port, function (err, done) {
-  if (err) {
-    console.log('error in listening');
-    return;
-  }
-  console.log('listening in port: ', config.port);
+    if (err) {
+        console.log('error in listening');
+        return;
+    }
+    console.log('listening in port: ', config.port);
 })
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/city', cityRoute);
+app.use('/department', deptRoute);
 
+app.use((req, res, next) => {
+    return next({
+        message: 'not found',
+        status: 404
+    })
+});
 
+app.use((err, req, res, next) => {
+    res.status(err.status || 400).json({
+        status: err.status || 400,
+        message: err.message
+    });
+})
 
